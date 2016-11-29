4cf9136f207f8ff5407f7ff4fdcefac512f3bb45
Dimas <dimsrad307@gmail.com>
2016-11-29 15:46:49 +0700

    designed basic ui

---
+++ Rocket.js
@@ -0,0 +1,12 @@
+'use strict'
+import React from 'react'
+import Image from 'react-native'
+
+
+export default  React.createClass ({
+  render(){
+    return(
+      <Image source={{uri: 'https://s3.amazonaws.com/vigesharing-is-vigecaring/lkurtz/rnwksp-c4-launch.jpg'}}/>
+    )
+  }
+})
